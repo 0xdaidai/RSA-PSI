bd8fded6897e42c107f748aee4fb9b2432f65e69
Psyduck0409 <rdwentao@163.com>
2020-06-14 15:41:37 +0800

    c-s update

--- client.c
+++ client.c
@@ -5,8 +5,8 @@
  > Created Time: Sat 13 Jun 2020 07:02:18 PM CST
  ************************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <gmp.h>
 #include "init.h"
 #include "rand.h"
@@ -14,55 +14,52 @@
 #include "phase.h"
 #include "file.h"
 
-int main(){
-    Public b;mpz_inits(b.n,b.e,NULL);
+int main()
+{
+    Public b;
+    mpz_inits(b.n, b.e, NULL);
     struct bloom bf;
 
     char send_buf[BUFFER_SIZE] = {0};
     char file_buf[1024];
     char *filename;
-    int clientSocket = connect_to_Server((char*)"127.0.0.1",SERVER_PORT);
-    // int clientSocket = connect_to_Server((char*)"39.96.57.195",SERVER_PORT);
-    if(clientSocket == -1) return 0;
+    int clientSocket = connect_to_Server((char *)"127.0.0.1", SERVER_PORT);
+    if (clientSocket == -1)
+        return 0;
     printf("已连接到服务器...\n");
-    printf("base qian\n");
-    _client_base(&b, 1000, "./resource/r.txt");
-    printf("base\n");
-    filename = receive_File(clientSocket);
-    rename(filename,"key-n.pub");
-    filename = receive_File(clientSocket);
-    rename(filename,"key-e.pub");
 
-
-    memset(file_buf,0,1024);
-    FILE *fp = fopen("./resource/pub-n.key","rb");
-    fseek(fp,0,SEEK_END); 
+    memset(file_buf, 0, 1024);
+    filename = receive_File(clientSocket);
+    FILE *fp = fopen(filename, "rb");
+    fseek(fp, 0, SEEK_END);
     int tt = ftell(fp);
-    fseek(fp,0,SEEK_SET);
-    fread(file_buf,1,tt,fp);
-    printf("b.n:%s\n", file_buf);       
-    mpz_set_str(b.n,file_buf,16);
+    fseek(fp, 0, SEEK_SET);
+    fread(file_buf, 1, tt, fp);
+    printf("b.n:%s\n", file_buf);
+    mpz_set_str(b.n, file_buf, 16);
 
-    memset(file_buf,0,1024);
-    fp = fopen("./resource/pub-e.key","rb");
-    fseek(fp,0,SEEK_END); 
+    memset(file_buf, 0, 1024);
+    filename = receive_File(clientSocket);
+    fp = fopen(filename, "rb");
+    fseek(fp, 0, SEEK_END);
     tt = ftell(fp);
-    fseek(fp,0,SEEK_SET);
-    fread(file_buf,1,tt,fp);
-    printf("b.e:%s\n", file_buf);       
-    mpz_set_str(b.e,file_buf,16);
+    fseek(fp, 0, SEEK_SET);
+    fread(file_buf, 1, tt, fp);
+    printf("b.e:%s\n", file_buf);
+    mpz_set_str(b.e, file_buf, 16);
 
+    printf("base start\n");
+    _client_base(&b, 1000, "./resource/r.txt");
+    printf("base finish\n");
 
     filename = receive_File(clientSocket);
-    _bloom_read(&bf,filename);
+    _bloom_read(&bf, filename);
 
     _client_online_send(&b, 1000, "./resource/y.txt", "./resource/r.txt", "./resource/a.txt");
-    printf("1\n");
-    submit_sel_File(clientSocket,"a.txt");
+    submit_sel_File(clientSocket, "a.txt");
     filename = receive_File(clientSocket);
     _client_online_receive(&b, 1000, &bf, filename, "./resource/r.txt", "./resource/y.txt", "./resource/s.txt");
 
-    mpz_clears(b.n,b.e,NULL);
+    mpz_clears(b.n, b.e, NULL);
     return 0;
-
 }
